45eb154b3b9a7c0748a1ae57bd11375988ec1f5a
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2022-03-18 14:46:23 +0000

    OCR app work going on

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.9-slim
+
+RUN apt-get update -y
+# RUN apt-get install -y python3-pip python-dev build-essential
+# RUN apt update && apt install -y libsm6 libxext6
+RUN apt-get -y install tesseract-ocr python3-opencv
+
+WORKDIR /app
+
+COPY ./requirements.txt /app/requirements.txt
+
+RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
+RUN pip install pillow
+RUN pip install pytesseract
+RUN pip install python-multipart
+RUN pip install opencv-contrib-python jinja2
+
+COPY ./src /app/src
+
+# CMD ["uvicorn", "src.main:app", "--host", "0.0.0.0", "--port", "80","--reload"]
---
+++ src/main.py
@@ -0,0 +1,68 @@
+from typing import Optional
+
+from fastapi import FastAPI, status, File, UploadFile, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+import io
+import base64
+import cv2
+import time
+import numpy as np
+import pytesseract
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
+templates = Jinja2Templates(directory="src/templates")
+
+
+def get_result(image_file):
+    start_time = time.time()
+    image_bytes = image_file.file.read()
+    decoded_img = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), -1)
+    text = pytesseract.image_to_string(decoded_img)
+    encoded_string = base64.b64encode(image_bytes)
+    bs64 = encoded_string.decode('utf-8')
+    image_data = f'data:image/jpeg;base64,{bs64}'
+    e_time = "--- %.5f seconds ---" % (time.time() - start_time)
+    return {
+        "text": text,
+        "inference_time": e_time,
+        "message": "success",
+        "status": status.HTTP_200_OK,
+        "image_data": image_data
+    }
+
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/")
+async def home_predict(request: Request, file: UploadFile = File(...)):
+    result = None
+    error = None
+    try:
+        result = get_result(image_file=file)
+
+    except Exception as ex:
+        error = ex
+    return templates.TemplateResponse("index.html", {"request": request, "result": result, "error": error})
+
+
+@app.get("/api")
+def read_root():
+    return {
+        "message": "Welcome to the API",
+        "status": status.HTTP_200_OK
+    }
+
+
+@app.post("/ocr")
+async def compute_ocr(request: Request, file: UploadFile = File(...)):
+    try:
+        return get_result(image_file=file)
+    except Exception as e:
+        return {"status": status.HTTP_400_BAD_REQUEST, "error": e.__str__(), }
